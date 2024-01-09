516e112df047ff479d3eb87ba6d79f489a96a373
MarkBidstrup <91070400+MarkBidstrup@users.noreply.github.com>
2024-01-09 11:12:30 +0100

    Added json dependency to pom.xml, client/server now creates json objects, send/receive them and save them to files in the JSON directory.

--- src/main/java/clientSide/ClientSocket.java
+++ src/main/java/clientSide/ClientSocket.java
@@ -1,5 +1,7 @@
 package clientSide;
 
+import org.json.JSONObject;
+
 import java.net.*;
 import java.io.*;
 
@@ -16,12 +18,33 @@ public class ClientSocket {
             BufferedReader inputFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter outputToServer = new PrintWriter(socket.getOutputStream(), true);
 
+            // Create a JSON object
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Column", "Backlog");
+            jsonObject.put("Name", "New task");
+            jsonObject.put("Description", "JSON sent from client");
+
             // Send data to server
             outputToServer.println("Hello, Server!");
 
             // Read server response
             String messageFromServer = inputFromServer.readLine();
             System.out.println("Server: " + messageFromServer);
+
+            // Read JSON data from server
+            String jsonString = inputFromServer.readLine();
+            System.out.println("Received from server: " + jsonString);
+
+            // Send JSON object to client
+            outputToServer.println(jsonObject.toString());
+
+            // Save JSON string to a JSON file
+            try (FileWriter fileWriter = new FileWriter("JSON/client_data.json")) {
+                fileWriter.write(jsonString);
+                System.out.println("JSON data saved to client_data.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             // Close resources
             inputFromServer.close();
--- src/main/java/serverSide/ServerSocket.java
+++ src/main/java/serverSide/ServerSocket.java
@@ -2,6 +2,7 @@ package serverSide;
 
 import java.net.*;
 import java.io.*;
+import org.json.*;
 
 public class ServerSocket {
     public static void main(String[] args) {
@@ -19,12 +20,33 @@ public class ServerSocket {
             BufferedReader inputFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter outputToClient = new PrintWriter(socket.getOutputStream(), true);
 
+            // Create a JSON object
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Column", "Backlog");
+            jsonObject.put("Name", "New task");
+            jsonObject.put("Description", "JSON sent from server");
+
             // Read data from client and display
             String messageFromClient;
             while ((messageFromClient = inputFromClient.readLine()) != null) {
                 System.out.println("Client: " + messageFromClient);
                 // Example response to client
                 outputToClient.println("Server received: " + messageFromClient);
+
+                // Send JSON object to client
+                outputToClient.println(jsonObject.toString());
+
+                // Read JSON data from server
+                String jsonString = inputFromClient.readLine();
+                System.out.println("Received from server: " + jsonString);
+
+                // Save JSON string to a JSON file
+                try (FileWriter fileWriter = new FileWriter("JSON/server_data.json")) {
+                    fileWriter.write(jsonString);
+                    System.out.println("JSON data saved to server_data.json");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             // Close resources
