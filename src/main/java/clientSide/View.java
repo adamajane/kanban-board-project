7ce14ee3aa77e32a68b7dbaa10ed45d38c8d258c
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2024-01-09 14:17:33 +0100

    Moved to Developer Branch

--- src/main/java/clientSide/View.java
+++ src/main/java/clientSide/View.java
@@ -1,4 +1,21 @@
 package clientSide;
 
-public class View {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UI.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("KanPlan");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
 }
