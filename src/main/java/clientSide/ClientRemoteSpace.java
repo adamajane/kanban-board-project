e13292d965f979e3d739c172319815ee8df57d70
adamajane <adamajane@gmail.com>
2024-01-11 11:40:34 +0100

    Client-server connection

--- src/main/java/clientSide/ClientRemoteSpace.java
+++ src/main/java/clientSide/ClientRemoteSpace.java
@@ -9,14 +9,23 @@ import java.util.Scanner;
 
 public class ClientRemoteSpace {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner scan = new Scanner(System.in);
-        RemoteSpace chat = new RemoteSpace("tcp://10.209.152.92:8080/backlog?keep");
 
-        while (true) {
-            String message = scan.nextLine();
-            chat.put(message);
-            String msg = Arrays.toString(chat.query(new FormalField(String.class)));
-            System.out.println(msg);
-        }
+        RemoteSpace backlog = new RemoteSpace("tcp://10.209.152.92:8080/backlog?keep");
+        RemoteSpace doing = new RemoteSpace("tcp://10.209.152.92:8080/doing?keep");
+        RemoteSpace review = new RemoteSpace("tcp://10.209.152.92:8080/review?keep");
+        RemoteSpace done = new RemoteSpace("tcp://10.209.152.92:8080/done?keep");
+
+        backlog.put("Tuple 1");
+        doing.put("Tuple 2");
+        review.put("Tuple 3");
+        done.put("Tuple 4");
+
+        // while (true) {
+        //     String message = scan.nextLine();
+        //     backlog.put(message);
+        //     String msg = Arrays.toString(backlog.query(new FormalField(String.class)));
+        //     System.out.println(msg);
+        // }
     }
+
 }
