b96790d924c77f800a8a0bf6837ac58a659d463d
adamajane <adamajane@gmail.com>
2024-01-11 13:11:57 +0100

    Adding tuples to remote spaces

--- src/main/java/clientSide/ClientRemoteSpace.java
+++ src/main/java/clientSide/ClientRemoteSpace.java
@@ -1,31 +1,63 @@
 package clientSide;
 
-import org.jspace.FormalField;
-import org.jspace.RemoteSpace;
+import org.jspace.*;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientRemoteSpace {
+
+    static RemoteSpace backlog;
+    static RemoteSpace doing;
+    static RemoteSpace review;
+    static RemoteSpace done;
+
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        RemoteSpace backlog = new RemoteSpace("tcp://10.209.152.92:8080/backlog?keep");
-        RemoteSpace doing = new RemoteSpace("tcp://10.209.152.92:8080/doing?keep");
-        RemoteSpace review = new RemoteSpace("tcp://10.209.152.92:8080/review?keep");
-        RemoteSpace done = new RemoteSpace("tcp://10.209.152.92:8080/done?keep");
+        backlog = new RemoteSpace("tcp://10.209.152.92:8080/backlog?keep");
+        doing = new RemoteSpace("tcp://10.209.152.92:8080/doing?keep");
+        review = new RemoteSpace("tcp://10.209.152.92:8080/review?keep");
+        done = new RemoteSpace("tcp://10.209.152.92:8080/done?keep");
 
-        backlog.put("Tuple 1");
-        doing.put("Tuple 2");
-        review.put("Tuple 3");
-        done.put("Tuple 4");
-
+        //backlog.put("Tuple 1");
+        //add
+        //
+        // backlog.put("Tuple 9");
+        //doing.put("Tuple 2");
+        //review.put("Tuple 3");
+        //done.put("Tuple 4");
+        add(backlog, new Tuple("Tuple 5"));
+        welcomeScreen();
         // while (true) {
         //     String message = scan.nextLine();
         //     backlog.put(message);
         //     String msg = Arrays.toString(backlog.query(new FormalField(String.class)));
         //     System.out.println(msg);
         // }
+    }
+
+    public static void add(Space space, Tuple task) throws InterruptedException {
+        space.put(task.getTuple());
+        //System.out.println("Added: " + task + " to Backlog");
+    }
+
+    public static void welcomeScreen() throws InterruptedException {
+        System.out.println("Welcome to the KanPlan!");
+        System.out.println("Columns below");
+        //System.out.println("Backlog");
+        List<Object[]> taskList = backlog.queryAll(new FormalField(String.class));
+        System.out.println("Tasks in Backlog:");
+        for (Object[] obj : taskList) {
+            System.out.println(Arrays.toString(obj));
+        }
+
+
+        System.out.println("Please select an option:");
+        System.out.println("1. Add a task");
+        System.out.println("2. Move a task");
+        System.out.println("3. Exit");
     }
 
 }
