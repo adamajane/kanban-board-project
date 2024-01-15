493ea8f3c7567229a6512e00aa850de6aa3882b2
adamajane <adamajane@gmail.com>
2024-01-15 11:06:38 +0100

    Removed unused Driver classes

--- src/main/java/clientSide/Driver.java
+++
@@ -1,9 +0,0 @@
-package clientSide;
-
-public class Driver {
-    public static void main(String[] args) {
-        // testing client socket
-        ClientController clientController = new ClientController();
-        clientController.testRequest();
-    }
-}
